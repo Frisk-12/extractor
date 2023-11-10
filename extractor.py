f47bfaa7500ca71ab6b9b9b034928dd45570c289
Frisk-12 <126598650+Frisk-12@users.noreply.github.com>
2023-11-10 11:45:20 +0100

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -54,7 +54,7 @@ def main():
     st.write("Benvenuto in questa semplice applicazione!")
     
     system = """
-    Completa in formato json le seguenti info, prendendole e interpretando dal testo fornito dall'utente. In caso di nessuna informazione inserisci None.
+    Completa in formato json le seguenti info, prendendole e interpretando dal testo fornito dall'utente. In caso di nessuna informazione inserisci [] quando è rischiesta una lista oppure "" quando è richiesta una stringa.
 
     Cognome (str)
     Nome (str)
